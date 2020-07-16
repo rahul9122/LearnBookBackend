f309df13b7db4a7a436fa277533dfd21d9f5519d
shivraj kumar <shivrajkumarcu@gmail.com>
2020-07-16 12:43:17 +0530

    first commit

---
+++ demo/src/main/java/com/example/demo/LearnBookApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnBookApplication.class, args);
+	}
+
+}
